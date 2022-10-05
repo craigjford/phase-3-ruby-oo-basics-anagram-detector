9d12e53d04e65bc140345d4c55d3aaff8d89720f
Craig Ford <craig_ford@icloud.com>
2022-10-05 13:28:33 -0400

    commit tree a76889a52b414ed757786282fb70ea18b5209f81

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Anagram 
+      
+    attr_accessor = :word   
+    
+    def initialize(word)
+      @word = word
+    end   
+
+    def match(w_array)
+
+      result_arr = []
+
+      w_array.each do |w|
+        if w.length == @word.length
+          if w.chars.sort == @word.chars.sort
+            result_arr << w
+          end
+        end
+      end
+
+      result_arr
+      
+    end
+
+end
